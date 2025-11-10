7a3d4f3aba5141e350b1b973207e3f89ae23b8b5
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2025-11-10 16:19:15 +0530

    Fix HTML form and PHP connection logic
    Updated form submission logic and fixed HTML structure.

--- emp.php
+++ emp.php
@@ -1,45 +1,47 @@
 <html>
-    <head></head>
-    <title></title>
-    <body>
-        <form method="post" action="#">
-            enter id<input type="text" name="eid"><br>
-            enter name<input type="text" name="ename"><br>
-            enter dept<input type="text" name="dept"><br>
-            enter salary<input type="text" name="sal"><br><br>
-            <input type="submit" name="submit">
-        </from>
-    </body>
+<head></head>
+<title></title>
+<body>
+    <form method="post" action="">
+        enter id <input type="text" name="eid"><br>
+        enter name <input type="text" name="ename"><br>
+        enter dept <input type="text" name="dept"><br>
+        enter salary <input type="text" name="sal"><br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
 </html>
+
 <?php
 $servername="localhost";
-$username="root";$password="";
+$username="root";
+$password="";
 $dbname="empdb";
-$tbname="emp";
+
 $conn=mysqli_connect($servername,$username,$password,$dbname);
-if(!$conn)
-{
-    die("Connection failed".mysqli_connect_error());
+
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+} else {
+    echo "<br><h2 align=center>connected...</h2><br>";
 }
-else
-{
-    echo"<br><h2 align=center>connected...</h2><br/>";
+
+if(isset($_POST['submit'])) {
+
+    $id = $_POST['eid'];
+    $name = $_POST['ename'];
+    $dept = $_POST['dept'];
+    $salary = $_POST['sal'];
+
+    $query = "INSERT INTO emp(eid,ename,dept,sal) VALUES('$id','$name','$dept','$salary')";
+    $res = mysqli_query($conn,$query);
+
+    if($res){
+        echo "SUBMITTED SUCCESSFULLY!!";
+    } else {
+        echo "ERROR: " . mysqli_error($conn);
+    }
 }
-$id=$_POST['eid'];
-$name=$POST['ename'];
-$dep=$_POST['dept'];
-$salary=$_POST['sal'];
-//$sql=mysqli_select_db("$dbname")die("Couldnt select db");
-$query="INSERT INTO emp(eid,ename,dept,sal)
-VALUES('".$id."','".$name."','".$dept."','".$salary."')";
-$res=mysqli_query($conn,$query);
-if($res)
-{
-    echo "SUBMITTED SUCCESSFULLY!!";
-}
-else
-{
-    echo "ERROR";
-}
+
 mysqli_close($conn);
 ?>
